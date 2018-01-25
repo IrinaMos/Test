88d89d53649ec7c82128712e7972cbf0854198bd
Irina Moskovich <irena.moskovich@gmail.com>
2018-01-25 14:49:20 +0200

    2.3. Элементарный калькулятор. [#185]

---
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -0,0 +1,30 @@
+package ru.job4j.calculator;
+
+import ru.job4j.Calculate;
+
+public class Calculator {
+
+	private double result;
+
+		public void add (double first, double second){
+			this.result = first + second;
+		}
+
+		public void subtract ( double first1, double second1){
+			this.result = first1 - second1;
+		}
+
+		public void div ( double first2, double second2){
+			this.result = first2 / second2;
+		}
+
+		public void multiple ( double first3, double second3){
+			this.result = first3 * second3;
+		}
+
+		public double getResult()
+		{
+			return this.result;
+		}
+
+	}
---
+++ chapter_001/src/test/java/ru/job4j/calculator/CalculatorTest.java
@@ -0,0 +1,41 @@
+package ru.job4j.calculator;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class CalculatorTest {
+    @Test
+    public void whenAddOnePlusOneThenTwo() {
+        Calculator calc = new Calculator();
+        calc.add(1, 1);
+        double result = calc.getResult();
+        double expected = 2;
+        assertThat(result, is(expected));
+    }
+        @Test
+        public void whenSubstracFirstValueMinusSecondValue () {
+            Calculator calc1 = new Calculator();
+            calc1.subtract(1, 1);
+            double result01 = calc1.getResult();
+            double expected01 = 0;
+            assertThat(result01, is(expected01));
+        }
+        @Test
+        public void whenDivFirstValueANDSecondValue() {
+                Calculator calc2 = new Calculator();
+                calc2.div(1, 1);
+                double result = calc2.getResult();
+                double expected = 1;
+                assertThat(result, is(expected));
+            }
+        @Test
+        public void whenMultiFirstValueANDSecondValue() {
+                Calculator calc3 = new Calculator();
+                calc3.multiple(1, 1);
+                double result = calc3.getResult();
+                double expected = 1;
+                assertThat(result, is(expected));
+                 }
+}
