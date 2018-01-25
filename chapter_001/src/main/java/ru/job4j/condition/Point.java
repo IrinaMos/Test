627111935bdd6f366b84a2883b3725dedf8a6b51
Irina Moskovich <irena.moskovich@gmail.com>
2018-01-25 16:05:21 +0200

    2.4. Расстояние между точками в системе координат. [#188]

---
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -0,0 +1,56 @@
+package ru.job4j.condition;
+
+public class Point {
+    private int x;
+    private int y;
+
+    public  Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distanceTo(Point that) {
+        // Точка А - это текущая точка. К ней мы обращаемся через оператор this.
+        Point a = this;
+        // Точка В - это входящая точка. К ней мы можем обратиться напрямую через имя переменной that.
+        // или для удоства мы создали новую переменню b и к ней присвоили переменную this.
+        Point b = that;
+        // сделаем вывод на консоль.
+        System.out.println("x1 = " + a.x);
+        System.out.println("y1 = " + a.y);
+        System.out.println("x2 = " + b.x);
+        System.out.println("y2 = " + b.y);
+        int x1 = a.x;
+        int y1 = a.y;
+        int x2 = b.x;
+        int y2 = b.y;
+//        System.out.println(x1);
+//        System.out.println(y1);
+//        System.out.println(x2);
+//        System.out.println(y2);
+
+        double result = Math.sqrt(
+                Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)
+        );
+        return result;
+    }
+
+    //   public double distanceTo(Point that) {
+    //    return Math.sqrt(
+    //           Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2)
+    //    );
+    //  }
+
+    public static void main(String[] args) {
+        Point a = new Point(0, 1);
+        Point b = new Point(2, 5);
+        //       a.distanceTo(b);
+        //System.out.println("x1 = " + a.x);
+        //System.out.println("y1 = " + a.y);
+        //System.out.println("x2 = " + b.x);
+        //System.out.println("y2 = " + b.y);
+// сделаем вызов метода
+        double result = a.distanceTo(b);
+        System.out.println("Расстояние между точками А и В : " + result);
+    }
+}
