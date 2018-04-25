a6c56c2ea9deee3352160df442e8f8d12a0b55f0
Irina Moskovich <irena.moskovich@gmail.com>
2018-04-25 17:46:35 +0300

    3.2. Максимум из двух чисел [#189]

---
+++ max/src/main/java/ru/job4j/Max.java
@@ -0,0 +1,20 @@
+package ru.job4j;
+
+/**
+ * @author Irina Moskovich (irena.moskovich@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Max {
+
+	public int max(int first, int second) {
+        if (first > second) {
+            return (first);
+        }
+        return (second);
+    }
+
+//	public int summation(int first, int second){
+//            return first + second;
+//        }
+}
